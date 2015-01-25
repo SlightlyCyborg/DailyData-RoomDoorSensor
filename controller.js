26c0045a7ef1ae6fea81a7d0c513cbea3c78d691
Collin Bell <slightlycyborg@gmail.com>
2015-01-25 13:00:41 -0500

    finished, db will be created later

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
 var SerialPort = require("serialport").SerialPort
+var fs = require('fs');
 var arduino = new SerialPort("/dev/ttyACM0", {
 	  baudrate: 9600
 });
@@ -13,17 +14,42 @@ arduino.on("open", function () {
 		}else{
 			d += data;
 			d = d.toString().slice(0,d.length-2);
-			if (d == "opened"){
-				//SAVE DATA HERE
-			}
-			if (d == "closed"){
-				//SAVE DATA HERE
-			}
+			//SAVE DATA HERE
+			to_write = d +', ' + getDateTime() + '\n';
+			fs.appendFile("../data/br_door.dat", to_write, function(err) {
+				if(err) {
+					console.log(err);
+				} 
+			}); 
 			d = "";
-			
 		}
 	});
 });
+
+function getDateTime() {
+
+	var date = new Date();
+
+	var hour = date.getHours();
+	hour = (hour < 10 ? "0" : "") + hour;
+
+	var min  = date.getMinutes();
+	min = (min < 10 ? "0" : "") + min;
+
+	var sec  = date.getSeconds();
+	sec = (sec < 10 ? "0" : "") + sec;
+
+	var year = date.getFullYear();
+
+	var month = date.getMonth() + 1;
+	month = (month < 10 ? "0" : "") + month;
+
+	var day  = date.getDate();
+	day = (day < 10 ? "0" : "") + day;
+
+	return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
 
 
 
